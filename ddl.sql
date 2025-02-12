77dea5653c4175847fe4bce9202924e3d6ed619d
Caio <104801828+yKaayo@users.noreply.github.com>
2025-02-12 09:53:42 -0300

    Tables Added

--- ddl.sql
+++ ddl.sql
@@ -22,3 +22,48 @@ CREATE TABLE cidade (
 ALTER TABLE cidade
     ADD CONSTRAINT fk_cidade FOREIGN KEY ( id_estado )
         REFERENCES estado ( id_estado );
+
+CREATE TABLE bairro (
+    id_bairro   NUMBER PRIMARY KEY,
+    nome_bairro VARCHAR(30) NOT NULL,
+    id_cidade   NUMBER
+);
+
+ALTER TABLE bairro
+    ADD CONSTRAINT fk_bairro FOREIGN KEY ( id_cidade )
+        REFERENCES cidade ( id_cidade );
+
+CREATE TABLE endereco (
+    id_endereco   NUMBER PRIMARY KEY,
+    nome_endereco VARCHAR(30) NOT NULL,
+    referencia    VARCHAR(50),
+    cep           NUMBER NOT NULL,
+    numero        NUMBER NOT NULL,
+    id_bairro     NUMBER
+);
+
+ALTER TABLE endereco
+    ADD CONSTRAINT fk_endereco FOREIGN KEY ( id_bairro )
+        REFERENCES bairro ( id_bairro );
+
+CREATE TABLE tipo_logadouro (
+    id_tipo_log   NUMBER PRIMARY KEY,
+    descricao_log VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE cliente (
+    id_cliente         NUMBER PRIMARY KEY,
+    nome               VARCHAR(50) NOT NULL,
+    data_de_nascimento DATE NOT NULL,
+    cpf_cnpj           NUMBER NOT NULL
+);
+
+CREATE TABLE tipo_cliente (
+    id_tipo_cliente NUMBER PRIMARY KEY,
+    descricao_tipo  VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE genero (
+    id_genero        NUMBER PRIMARY KEY,
+    descricao_genero VARCHAR(30) NOT NULL
+);
